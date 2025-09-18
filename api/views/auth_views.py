050debd1af2f90522f24c4a18d5872985d67e9bf
Mahmoud Darawsheh <darxx03eh@mail.ru>
2025-09-18 11:01:03 +0300

    test conflict main

--- api/views/auth_views.py
+++ api/views/auth_views.py
@@ -10,6 +10,11 @@ from api.serializers.auth_serializers import (
     SignupSerializer,
 )
 import logging
+
+def conflict():
+    pass
+
+# this is to test conflict
 logger = logging.getLogger('api.controllers')
 @extend_schema(tags=["auth"])
 class LoginAPIView(TokenObtainPairView):
@@ -24,6 +29,11 @@ class LoginAPIView(TokenObtainPairView):
         except Exception as e:
             logger.error(f'Failed login attempt for user={username_or_email}. Error: {str(e)}')
             raise
+
+def conflict():
+    pass
+
+# this is to test conflict
 
 class SignupAPIView(APIView):
     serializer_class = SignupSerializer
